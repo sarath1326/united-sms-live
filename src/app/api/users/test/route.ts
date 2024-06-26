e41b104b2695b5ed049e9c73cbd7a5654e7864ae
sarath <sarathsarath93366@gmail.com>
2024-06-26 21:20:35 +0530

    ok 5

--- src/app/api/users/test/route.ts
+++ src/app/api/users/test/route.ts
@@ -1,11 +1,29 @@
 
+import {DBconnecting} from "@/DBconfig/Dbconn"
+import {sparesschema}  from "@/models/spares"
 
 import { NextRequest , NextResponse} from "next/server"
 
 
 export async function GET(req:NextRequest){
 
-     console.log("req test")
+    
+    try {
+
+        
+        DBconnecting()
+
+        console.log("req test")
+
+        const result= await sparesschema.find()
+
+
   
-     return NextResponse.json({msg:"good luck"})
+     return NextResponse.json({msg:"good luck",data:result})
+        
+    } catch (error) {
+        
+    }
+
+     
 }
