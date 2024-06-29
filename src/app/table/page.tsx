ea2030460a30046a8eea59401a4365caf79613e1
sarath <sarathsarath93366@gmail.com>
2024-06-29 21:53:55 +0530

    set ok table ok 1.0

--- src/app/table/page.tsx
+++ src/app/table/page.tsx
@@ -1,93 +1,93 @@
 
 
-"use client"
+// "use client"
 
-import React from 'react'
-import { useState } from 'react'
-
-
-function Page() {
-
-    const [th, setth] = useState([
-
-        "Part Name",
-        "Part Code ",
-        " Customer Name",
-        "Customer Number",
-        "Tech Name",
-        "Warranty Status",
-        " Company",
-        "Receving Date",
-        "Part Sent / Payment",
-        "Delete"
-
-    ])
+// import React from 'react'
+// import { useState } from 'react'
 
 
-    const [data,setdata]=useState([1,2,3,4,5,6,7,8,9,10])
+// function Page() {
+
+//     const [th, setth] = useState([
+
+//         "Part Name",
+//         "Part Code ",
+//         " Customer Name",
+//         "Customer Number",
+//         "Tech Name",
+//         "Warranty Status",
+//         " Company",
+//         "Receving Date",
+//         "Part Sent / Payment",
+//         "Delete"
+
+//     ])
+
+
+//     const [data,setdata]=useState([1,2,3,4,5,6,7,8,9,10])
   
   
-    return (
-    <div>
+//     return (
+//     <div>
 
-        <h1 className='text-center' > Table Demo </h1>
+//         <h1 className='text-center' > Table Demo </h1>
 
-        <div className=' w-full overflow-x-auto ' >
+//         <div className=' w-full overflow-x-auto ' >
 
-            <table className='w-full'>
+//             <table className='w-full'>
 
-                <thead>
-                    <tr className='border-2 border-gray-500' >
+//                 <thead>
+//                     <tr className='border-2 border-gray-500' >
 
-                        {
-                             th.map((obj)=>(
+//                         {
+//                              th.map((obj)=>(
 
-                                  <th className='px-6 py-3 ' > {obj} </th>
-                             ))
-                        }
-
-
-                    </tr>
+//                                   <th className='px-6 py-3 ' > {obj} </th>
+//                              ))
+//                         }
 
 
-                </thead>
+//                     </tr>
 
-                <tbody>
 
-                    <tr>
+//                 </thead>
 
-                        {
-                             data.map((obj)=>(
+//                 <tbody>
+
+//                     <tr>
+
+//                         {
+//                              data.map((obj)=>(
 
                               
-                                    <td className='px-6 py-1'>
-                                        {obj}
-                                    </td>
+//                                     <td className='px-6 py-1'>
+//                                         {obj}
+//                                     </td>
                                
 
                                   
-                             ))
-                        }
+//                              ))
+//                         }
 
-                        </tr>
+//                         </tr>
                         
 
                     
 
 
-                </tbody>
+//                 </tbody>
 
 
-            </table>
+//             </table>
 
 
-        </div>
+//         </div>
 
 
 
       
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-export default Page
+// export default Page
